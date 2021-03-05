0f9ce8c862965313ed9935a717bdebb13d66f1c5
Tom Irving <info@thermoglobalnuclearwar.com>
2021-03-05 14:12:58 +0000

    remove play record

--- AudioDemo/ViewController.swift
+++ AudioDemo/ViewController.swift
@@ -8,28 +8,16 @@ class ViewController: UIViewController {
         let view = UIStackView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.axis = .horizontal
-        view.spacing = 20
+        view.spacing = 10
         return view
     }()
     
-    private lazy var recordButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Record", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.setTitleColor(.black, for: .highlighted)
-        button.addTarget(self, action: #selector(recordButtonWasTapped(_:)), for: .touchUpInside)
-        return button
-    }()
-    
-    private lazy var playButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Play", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.setTitleColor(.black, for: .highlighted)
-        button.addTarget(self, action: #selector(playButtonWasTapped(_:)), for: .touchUpInside)
-        return button
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .label
+        label.text = "Measurement Mode: "
+        return label
     }()
     
     private lazy var measureModeSwitch: UISwitch = {
@@ -54,17 +42,8 @@ class ViewController: UIViewController {
             self.stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
         ])
         
-        self.stackView.addArrangedSubview(self.recordButton)
-        self.stackView.addArrangedSubview(self.playButton)
+        self.stackView.addArrangedSubview(self.label)
         self.stackView.addArrangedSubview(self.measureModeSwitch)
-    }
-    
-    @objc private func recordButtonWasTapped(_ sender: UIButton) {
-        print("Record button was tapped")
-    }
-    
-    @objc private func playButtonWasTapped(_ sender: UIButton) {
-        print("Play button was tapped")
     }
     
     @objc private func measureModeSwitchWasToggled(_ sender: UISwitch) {
