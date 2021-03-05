0dbf3372e00d64a787169c0bca8956eb0e5e8966
Tom Irving <info@thermoglobalnuclearwar.com>
2021-03-05 12:50:13 +0000

    Add measure mode toggle

--- AudioDemo/ViewController.swift
+++ AudioDemo/ViewController.swift
@@ -29,6 +29,13 @@ class ViewController: UIViewController {
         button.addTarget(self, action: #selector(playButtonWasTapped(_:)), for: .touchUpInside)
         return button
     }()
+    
+    private lazy var measureModeSwitch: UISwitch = {
+        let modeSwitch = UISwitch()
+        modeSwitch.translatesAutoresizingMaskIntoConstraints = false
+        modeSwitch.addTarget(self, action: #selector(measureModeSwitchWasToggled(_:)), for: .valueChanged)
+        return modeSwitch
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,6 +52,7 @@ class ViewController: UIViewController {
         
         self.stackView.addArrangedSubview(self.recordButton)
         self.stackView.addArrangedSubview(self.playButton)
+        self.stackView.addArrangedSubview(self.measureModeSwitch)
     }
     
     @objc private func recordButtonWasTapped(_ sender: UIButton) {
@@ -53,5 +61,9 @@ class ViewController: UIViewController {
     
     @objc private func playButtonWasTapped(_ sender: UIButton) {
         print("Play button was tapped")
+    }
+    
+    @objc private func measureModeSwitchWasToggled(_ sender: UISwitch) {
+        print("Measure mode isOn: \(sender.isOn)")
     }
 }
